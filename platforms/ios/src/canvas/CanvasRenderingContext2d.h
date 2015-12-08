e6012b1e2cf847b8cd4b86bd5c55f97d115ef34f
Stefan Dragnev <dragnev@dropboxsyncpc.telerik.com>
2015-12-08 14:37:45 +0200

    ios platform module

---
+++ platforms/ios/src/canvas/CanvasRenderingContext2d.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+@interface __CanvasRenderingContext2D : NSObject
+-(void)setWidth:(int)width height:(int)height;
+-(void)fillRectX:(float)x Y:(float)y W:(float)width H:(float)height;
+@end
